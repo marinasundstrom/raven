2f3cfdb241cae19db85832fc04cd1347df63f7ae
Marina Sundström <marna.li@outlook.com>
2024-12-15 23:14:09 +0100

    Update sample

--- Raven.Compiler/Program.cs
+++ Raven.Compiler/Program.cs
@@ -5,10 +5,12 @@ var fileName = args.Length > 0 ? args[0] : "../../../test.rav";
 
 var sourceText = SourceText.From(File.OpenRead(fileName));
 
-var syntaxTree = SyntaxTree.ParseText(sourceText);
+var syntaxTree = SyntaxFactory.ParseSyntaxTree(sourceText);
 
 var root = syntaxTree.GetRoot();
 
-//Console.WriteLine(root.ToFullString());
-
 Console.WriteLine(root.GetSyntaxTreeRepresentation(includeTokens: true, includeTrivia: false));
+
+Console.WriteLine();
+
+Console.WriteLine(root.ToFullString());
