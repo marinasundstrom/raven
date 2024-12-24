6b1f669ebc23757593824273a23abf843ef8fc50
Marina Sundström <marna.li@outlook.com>
2024-12-24 14:46:38 +0100

    Remove names in enum

--- Raven.CodeAnalysis/ISymbol.cs
+++ Raven.CodeAnalysis/ISymbol.cs
@@ -44,12 +44,9 @@ public enum Accessibility
 {
     NotApplicable = 0,
     Private = 1,
-    ProtectedAndFriend = 2,
     ProtectedAndInternal = 2,
     ProtectedAndProtected = 3,
-    Friend = 4,
     Internal = 4,
-    ProtectedOrFriend = 5,
     ProtectedOrInternal = 5,
     Public = 6,
 }
