0949846eb1b90f8364071e2bb177fbd40177bc2a
Marina Sundström <marna.li@outlook.com>
2024-12-25 17:29:42 +0100

    Add SyntaxNode tests

--- Raven.CodeAnalysis.Tests/Syntax/SyntaxNodeTest.cs
+++ Raven.CodeAnalysis.Tests/Syntax/SyntaxNodeTest.cs
@@ -1,4 +1,6 @@
-﻿using Xunit;
+﻿using Shouldly;
+
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Raven.CodeAnalysis.Syntax.Tests;
@@ -8,7 +10,63 @@ using static Raven.CodeAnalysis.Syntax.SyntaxFactory;
 public class SyntaxNodeTest(ITestOutputHelper testOutputHelper)
 {
     [Fact]
-    public void Test1()
+    public void ReplaceNode()
+    {
+        var returnStatement = ReturnStatement(ReturnKeyword,
+                        LiteralExpression(SyntaxKind.NumericLiteralExpression, NumericLiteral(42)),
+                        SemicolonToken);
+
+        var literalExpression = returnStatement
+            .ChildNodes()
+            .OfType<LiteralExpressionSyntax>().First();
+
+        var newLiteralExpression = LiteralExpression(SyntaxKind.StringLiteralToken, IdentifierToken("test"));
+
+        var newReturnStatement = returnStatement.ReplaceNode(literalExpression, newLiteralExpression);
+
+        newReturnStatement.ShouldNotBeSameAs(returnStatement);
+
+        testOutputHelper.WriteLine(newReturnStatement.ToFullString());
+    }
+
+    [Fact]
+    public void ReplaceToken()
+    {
+        var returnStatement = ReturnStatement(ReturnKeyword,
+                        LiteralExpression(SyntaxKind.NumericLiteralExpression, NumericLiteral(42)),
+                        SemicolonToken);
+
+        var literalExpression = returnStatement
+            .ChildNodes()
+            .OfType<LiteralExpressionSyntax>().First();
+
+        var newToken = IdentifierToken("test");
+
+        var newReturnStatement = returnStatement.ReplaceToken(literalExpression.Token, newToken);
+
+        newReturnStatement.ShouldNotBeSameAs(returnStatement);
+
+        testOutputHelper.WriteLine(newReturnStatement.ToFullString());
+    }
+
+    [Fact]
+    public void SyntaxNode_WithNoTrivia()
+    {
+        var block = Block(
+            OpenBraceToken,
+            List<StatementSyntax>(
+                ReturnStatement(ReturnKeyword,
+                    LiteralExpression(SyntaxKind.NumericLiteralExpression, NumericLiteral(42)),
+                    SemicolonToken)
+            ),
+            CloseBraceToken
+        );
+
+        testOutputHelper.WriteLine(block.ToFullString());
+    }
+
+    [Fact]
+    public void SyntaxNode_WithTrivia()
     {
         var block = Block(
             OpenBraceToken
@@ -24,16 +82,52 @@ public class SyntaxNodeTest(ITestOutputHelper testOutputHelper)
                 .WithTrailingTrivia(LineFeed)
         );
 
-        var x = block.Statements[0];
+        testOutputHelper.WriteLine(block.ToFullString());
+    }
 
-        foreach (var nodeOrToken in block.ChildNodesAndTokens())
-        {
-            testOutputHelper.WriteLine($"{nodeOrToken.Node?.Kind ?? nodeOrToken.Token.Kind} [{nodeOrToken.Node?.FullSpan ?? nodeOrToken.Token.FullSpan}]");
-        }
+    [Fact]
+    public void ReplaceTokenWithTokenWithTrivia()
+    {
+        var block = Block(
+            OpenBraceToken,
+            List<StatementSyntax>(
+                ReturnStatement(ReturnKeyword,
+                    LiteralExpression(SyntaxKind.NumericLiteralExpression, NumericLiteral(42)),
+                    SemicolonToken)
+            ),
+            CloseBraceToken
+        );
 
-        var str = block.ToString();
-        var str2 = block.ToFullString();
+        var newBlock = block.ReplaceToken(block.OpenBraceToken, block.OpenBraceToken.WithTrailingTrivia(SyntaxFactory.CarriageReturnLineFeed));
 
-        testOutputHelper.WriteLine(str2);
+        newBlock.ShouldNotBeSameAs(block);
+
+        testOutputHelper.WriteLine(block.ToFullString());
+        testOutputHelper.WriteLine(newBlock.ToFullString());
+    }
+
+    [Fact]
+    public void ReplaceNodeInListWithNode()
+    {
+        var block = Block(
+            OpenBraceToken,
+            List<StatementSyntax>(
+                ReturnStatement(ReturnKeyword,
+                    LiteralExpression(SyntaxKind.NumericLiteralExpression, NumericLiteral(42)),
+                    SemicolonToken)
+            ),
+            CloseBraceToken
+        );
+
+        var returnStatement = block.Statements.OfType<ReturnStatementSyntax>().First();
+
+        var newChild = ExpressionStatement(LiteralExpression(SyntaxKind.NumericLiteralExpression, NumericLiteral(20)));
+
+        var newBlock = block.ReplaceNode(returnStatement, newChild);
+
+        newBlock.ShouldNotBeSameAs(block);
+
+        testOutputHelper.WriteLine(block.ToFullString());
+        testOutputHelper.WriteLine(newBlock.ToFullString());
     }
 }
