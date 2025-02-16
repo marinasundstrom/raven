5e2fccc2ea43eec0b2e678e8ba7fba4a623ac2ed
Marina Sundström <marna.li@outlook.com>
2025-02-16 23:23:16 +0100

    Add test for single line comment trivia

---
+++ src/Raven.CodeAnalysis.Tests/Syntax/Parser/SingleLineCommentTriviaTest.cs
@@ -0,0 +1,43 @@
+namespace Raven.CodeAnalysis.Syntax.Parser.Tests;
+
+public class SingleLineCommentTriviaTest
+{
+    [Fact]
+    public void SingleLineCommentTrivia_IsLeadingTriviaOfToken()
+    {
+        var code = """
+                    if (foo)  {
+                        // Foo bar
+                        return 0;
+                    }
+                    """;
+
+        var syntaxTree = SyntaxTree.ParseText(code);
+
+        var root = syntaxTree.GetRoot();
+
+        var returnStatement = root.DescendantNodes()
+            .OfType<ReturnStatementSyntax>()
+            .First();
+
+        var trivia = returnStatement.ReturnKeyword.LeadingTrivia.FirstOrDefault(x => x.Kind == SyntaxKind.SingleLineCommentTrivia);
+
+        trivia.ShouldNotBeNull();
+    }
+
+    [Fact(Skip = "Because of issue #88")]
+    public void SingleLineCommentTrivia_IsLeadingTriviaOfEndOFileToken()
+    {
+        var code = """
+                    // Foo bar
+                    """;
+
+        var syntaxTree = SyntaxTree.ParseText(code);
+
+        var root = syntaxTree.GetRoot();
+
+        var trivia = root.EndOfFileToken.LeadingTrivia.FirstOrDefault(x => x.Kind == SyntaxKind.SingleLineCommentTrivia);
+
+        trivia.ShouldNotBeNull();
+    }
+}
