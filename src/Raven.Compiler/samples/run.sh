8b59f029934ac4688e418c946e1ac6a3e01f8ca7
Marina Sundström <marna.li@outlook.com>
2025-06-11 11:20:26 +0200

    Update sample

---
+++ src/Raven.Compiler/samples/run.sh
@@ -0,0 +1,8 @@
+dotnet output/arrays.dll
+dotnet output/enums.dll
+dotnet output/general.dll
+dotnet output/generics.dll
+dotnet output/io.dll .
+# dotnet output/test.dll 15
+dotnet output/test2.dll
+dotnet output/type-unions.dll
