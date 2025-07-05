09c6204b3cb9b5ff60d8bbd4cdf4184af87725bb
Marina Sundström <marna.li@outlook.com>
2025-07-05 21:48:51 +0200

    Remove migration tool

--- src/tools/SyntaxNodesToYamlDefMigrationTool/PropertyModel.cs
+++
@@ -1,8 +0,0 @@
-public class PropertyModel
-{
-    public string Name { get; set; } = string.Empty;
-    public string Type { get; set; } = string.Empty;
-    public bool Nullable { get; set; }
-    public bool Inherited { get; set; }
-    public bool Abstract { get; set; }
-}
--- src/tools/SyntaxNodesToYamlDefMigrationTool/SyntaxNodeModel.cs
+++
@@ -1,8 +0,0 @@
-public class SyntaxNodeModel
-{
-    public string Name { get; set; } = string.Empty;
-    public string Base { get; set; } = string.Empty;
-    public bool Abstract { get; set; }
-    public bool ExplicitKind { get; set; }
-    public List<PropertyModel> Properties { get; set; } = new();
-}
