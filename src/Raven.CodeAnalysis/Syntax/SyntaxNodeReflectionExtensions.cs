ac92499f0e67bda7e656cc7e679728316eb4e258
Marina Sundström <marna.li@outlook.com>
2025-07-08 08:52:28 +0200

    Add class ChildGroup

--- src/Raven.CodeAnalysis/Syntax/SyntaxNodeReflectionExtensions.cs
+++ src/Raven.CodeAnalysis/Syntax/SyntaxNodeReflectionExtensions.cs
@@ -90,45 +90,52 @@ public static class SyntaxNodeReflectionExtensions
         return null; // No matching property found
     }
 
-    public static IEnumerable<IGrouping<string, SyntaxNodeOrToken>> GetChildrenGroupedByPropertyName(this SyntaxNode node, bool includeTokens = true)
+    public static IEnumerable<ChildGroup> GetChildrenGroupedByProperty(this SyntaxNode node, bool includeTokens = true)
     {
         var children = node.ChildNodesAndTokens();
-        var groups = new List<(string Name, SyntaxNodeOrToken Child)>();
+        var groups = new Dictionary<string, List<SyntaxNodeOrToken>>();
+
+        var listProperties = new HashSet<string>();
 
         foreach (var child in children)
         {
             string? name = null;
+            bool isListItem = child.ParentListGreen is not null;
 
             if (child.IsNode && child.AsNode() is SyntaxNode childNode)
             {
-                if (child.ParentListGreen is not null)
-                {
-                    name = childNode.Parent?.GetPropertyNameForListItem(childNode);
-
-                }
-                else
-                {
-                    name = childNode.Parent?.GetPropertyNameForChild(childNode);
-                }
+                name = isListItem
+                    ? childNode.Parent?.GetPropertyNameForListItem(childNode)
+                    : childNode.Parent?.GetPropertyNameForChild(childNode);
             }
             else if (includeTokens && child.IsToken && child.AsToken() is SyntaxToken token)
             {
-                if (child.ParentListGreen is not null)
-                {
-                    name = token.Parent?.GetPropertyNameForListItem(token);
-
-                }
-                else
-                {
-                    name = token.Parent?.GetPropertyNameForChild(token);
-                }
+                name = isListItem
+                    ? token.Parent?.GetPropertyNameForListItem(token)
+                    : token.Parent?.GetPropertyNameForChild(token);
             }
 
             if (!string.IsNullOrEmpty(name))
-                groups.Add((name!, child));
+            {
+                if (!groups.TryGetValue(name!, out var list))
+                    groups[name!] = list = new List<SyntaxNodeOrToken>();
+
+                list.Add(child);
+
+                if (isListItem)
+                    listProperties.Add(name!);
+            }
         }
 
-        return groups.GroupBy(x => x.Name, x => x.Child);
+        foreach (var kvp in groups)
+        {
+            yield return new ChildGroup
+            {
+                PropertyName = kvp.Key,
+                IsList = listProperties.Contains(kvp.Key),
+                Items = kvp.Value
+            };
+        }
     }
 
     public static string? GetPropertyNameForListItem(this SyntaxNode node, SyntaxNode nodeItem)
@@ -183,5 +190,12 @@ public static class SyntaxNodeReflectionExtensions
         }
 
         return null;
+    }
+
+    public sealed class ChildGroup
+    {
+        public required string PropertyName { get; init; }
+        public required bool IsList { get; init; }
+        public required IReadOnlyList<SyntaxNodeOrToken> Items { get; init; }
     }
 }
