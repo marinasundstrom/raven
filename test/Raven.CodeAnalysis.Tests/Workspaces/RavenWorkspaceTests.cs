17dad5cefcea46109d300531f38928c24c74e3c9
Marina Sundström <marna.li@outlook.com>
2025-08-19 10:28:07 +0200

    Add test

--- test/Raven.CodeAnalysis.Tests/Workspaces/RavenWorkspaceTests.cs
+++ test/Raven.CodeAnalysis.Tests/Workspaces/RavenWorkspaceTests.cs
@@ -1,4 +1,6 @@
 using Raven.CodeAnalysis;
+using Raven.CodeAnalysis.Text;
+
 using Xunit;
 
 namespace Raven.CodeAnalysis.Tests;
@@ -10,6 +12,21 @@ public class RavenWorkspaceTests
     {
         var workspace = RavenWorkspace.Create();
         var projectId = workspace.AddProject("App");
+
+        var compilation = workspace.GetCompilation(projectId);
+        Assert.NotEmpty(compilation.References);
+    }
+
+    [Fact]
+    public void AddProjectAndDocument_ShouldIncludeDocumentInProject()
+    {
+        var workspace = RavenWorkspace.Create();
+        var projectId = workspace.AddProject("App");
+
+        workspace.CurrentSolution.AddDocument(
+                DocumentId.CreateNew(projectId),
+                "test.rav",
+                SourceText.From("System.Console.WriteLine(\"Hello\")\n"));
 
         var compilation = workspace.GetCompilation(projectId);
         Assert.NotEmpty(compilation.References);
