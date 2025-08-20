0ba3d1cf9458ffaeb4776ae8a349ab84d0246cd3
Marina Sundström <marna.li@outlook.com>
2025-08-20 22:26:42 +0200

    fix: load metadata references before type lookup (#175)

---
+++ test/Raven.CodeAnalysis.Tests/Bugs/MetadataReferenceLoadingTests.cs
@@ -0,0 +1,22 @@
+using Raven.CodeAnalysis;
+
+namespace Raven.CodeAnalysis.Tests.Bugs;
+
+public class MetadataReferenceLoadingTests
+{
+    [Fact]
+    public void GetTypeByMetadataName_LoadsReferences_WhenNoSyntaxTrees()
+    {
+        var referencePaths = ReferenceAssemblyPaths.GetReferenceAssemblyPaths();
+        var references = referencePaths.Select(MetadataReference.CreateFromFile).ToArray();
+
+        var compilation = Compilation.Create("test", new CompilationOptions(OutputKind.ConsoleApplication))
+            .AddReferences(references);
+
+        var consoleType = compilation.GetTypeByMetadataName("System.Console");
+        Assert.NotNull(consoleType);
+
+        var stringType = compilation.GetTypeByMetadataName("System.String");
+        Assert.NotNull(stringType);
+    }
+}
