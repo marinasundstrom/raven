57808cd8a4827d103e4341a033509eb106462274
Marina Sundström <marna.li@outlook.com>
2025-08-24 21:50:02 +0200

    Extract test

--- test/Raven.CodeAnalysis.Tests/Semantics/ImportResolutionTest.cs
+++ test/Raven.CodeAnalysis.Tests/Semantics/ImportResolutionTest.cs
@@ -112,35 +112,4 @@ public class ImportResolutionTest : DiagnosticTestBase
 
         verifier.Verify();
     }
-
-    [Fact]
-    public void AliasDirective_UsesAlias()
-    {
-        string testCode =
-            """
-            alias SB = System.Text.StringBuilder;
-
-            SB;
-            """;
-
-        var verifier = CreateVerifier(testCode);
-
-        verifier.Verify();
-    }
-
-    [Fact]
-    public void AliasDirective_UsesAlias_Generic()
-    {
-        string testCode =
-            """
-            alias IntList = System.Collections.Generic.List<int>;
-
-            IntList;
-            """;
-
-        var verifier = CreateVerifier(testCode);
-
-        verifier.Verify();
-    }
-
 }
