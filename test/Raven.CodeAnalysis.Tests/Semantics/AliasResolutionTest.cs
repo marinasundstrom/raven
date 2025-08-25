609a130d24d2af92fb7edd7e624ed16a1d182146
Marina Sundström <marna.li@outlook.com>
2025-08-25 22:28:10 +0200

    fix: support generic type aliases in annotations (#205)

--- test/Raven.CodeAnalysis.Tests/Semantics/AliasResolutionTest.cs
+++ test/Raven.CodeAnalysis.Tests/Semantics/AliasResolutionTest.cs
@@ -35,6 +35,21 @@ public class AliasResolutionTest : DiagnosticTestBase
     }
 
     [Fact]
+    public void AliasDirective_UsesAlias_AsTypeAnnotation()
+    {
+        string testCode =
+            """
+            alias StringList = System.Collections.Generic.List<string>
+
+            let list: StringList
+            """;
+
+        var verifier = CreateVerifier(testCode);
+
+        verifier.Verify();
+    }
+
+    [Fact]
     public void AliasDirective_UsesMemberAlias_Method()
     {
         string testCode =
