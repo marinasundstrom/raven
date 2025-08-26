c6201c2ae00d596806cee8d57545dc7e36149917
Marina Sundström <marna.li@outlook.com>
2025-08-26 09:28:02 +0200

    test: add byref constructor and local function tests

--- test/Raven.CodeAnalysis.Tests/Semantics/ByRefParameterTests.cs
+++ test/Raven.CodeAnalysis.Tests/Semantics/ByRefParameterTests.cs
@@ -5,6 +5,8 @@ using Raven.CodeAnalysis.Symbols;
 using Raven.CodeAnalysis.Syntax;
 using Raven.CodeAnalysis.Tests;
 
+using Xunit;
+
 namespace Raven.CodeAnalysis.Semantics.Tests;
 
 public class ByRefParameterTests
@@ -41,5 +43,88 @@ class C {
         var methodSymbol = (IMethodSymbol)model.GetDeclaredSymbol(method)!;
         var symbol = methodSymbol.Parameters.Single();
         Assert.Equal(RefKind.Out, symbol.RefKind);
+    }
+
+    [Fact]
+    public void ConstructorParameter_WithAmpersand_HasRefKindRef()
+    {
+        var source = """
+class C {
+    init(x: &int) { }
+}
+""";
+        var tree = SyntaxTree.ParseText(source);
+        var compilation = Compilation.Create("test", [tree], TestMetadataReferences.Default, new CompilationOptions(OutputKind.ConsoleApplication));
+        var model = compilation.GetSemanticModel(tree);
+        var ctor = tree.GetRoot().DescendantNodes().OfType<ConstructorDeclarationSyntax>().Single();
+        var ctorSymbol = (IMethodSymbol)model.GetDeclaredSymbol(ctor)!;
+        var parameter = ctorSymbol.Parameters.Single();
+        Assert.Equal(RefKind.Ref, parameter.RefKind);
+    }
+
+    [Fact]
+    public void Constructor_OutParameter_HasRefKindOut()
+    {
+        var source = """
+class C {
+    init(out x: &int) { x = 0 }
+}
+""";
+        var tree = SyntaxTree.ParseText(source);
+        var compilation = Compilation.Create("test", [tree], TestMetadataReferences.Default, new CompilationOptions(OutputKind.ConsoleApplication));
+        var model = compilation.GetSemanticModel(tree);
+        var ctor = tree.GetRoot().DescendantNodes().OfType<ConstructorDeclarationSyntax>().Single();
+        var ctorSymbol = (IMethodSymbol)model.GetDeclaredSymbol(ctor)!;
+        var parameter = ctorSymbol.Parameters.Single();
+        Assert.Equal(RefKind.Out, parameter.RefKind);
+    }
+
+    [Fact]
+    public void LocalFunctionParameter_WithAmpersand_HasRefKindRef()
+    {
+        var source = """
+func outer() {
+    func inner(x: &int) { }
+}
+""";
+        var tree = SyntaxTree.ParseText(source);
+        var compilation = Compilation.Create("test", [tree], TestMetadataReferences.Default, new CompilationOptions(OutputKind.ConsoleApplication));
+        var model = compilation.GetSemanticModel(tree);
+        var inner = tree.GetRoot().DescendantNodes().OfType<LocalFunctionStatementSyntax>().Single(l => l.Identifier.Text == "inner");
+        var symbol = (IMethodSymbol)model.GetDeclaredSymbol(inner)!;
+        var parameter = symbol.Parameters.Single();
+        Assert.Equal(RefKind.Ref, parameter.RefKind);
+    }
+
+    [Fact]
+    public void LocalFunction_OutParameter_HasRefKindOut()
+    {
+        var source = """
+func outer() {
+    func inner(out x: &int) { x = 1 }
+}
+""";
+        var tree = SyntaxTree.ParseText(source);
+        var compilation = Compilation.Create("test", [tree], TestMetadataReferences.Default, new CompilationOptions(OutputKind.ConsoleApplication));
+        var model = compilation.GetSemanticModel(tree);
+        var inner = tree.GetRoot().DescendantNodes().OfType<LocalFunctionStatementSyntax>().Single(l => l.Identifier.Text == "inner");
+        var symbol = (IMethodSymbol)model.GetDeclaredSymbol(inner)!;
+        var parameter = symbol.Parameters.Single();
+        Assert.Equal(RefKind.Out, parameter.RefKind);
+    }
+
+    [Fact]
+    public void Method_ByRefParameter_Passed_To_Other_ByRefMethod_Compiles()
+    {
+        var source = """
+class C {
+    static Set(x: &int) { x = 1 }
+    static Pass(x: &int) { Set(&x) }
+}
+""";
+        var tree = SyntaxTree.ParseText(source);
+        var compilation = Compilation.Create("test", [tree], TestMetadataReferences.Default, new CompilationOptions(OutputKind.ConsoleApplication));
+        var diagnostics = compilation.GetDiagnostics();
+        Assert.DoesNotContain(diagnostics, d => d.Descriptor.DefaultSeverity == DiagnosticSeverity.Error);
     }
 }
