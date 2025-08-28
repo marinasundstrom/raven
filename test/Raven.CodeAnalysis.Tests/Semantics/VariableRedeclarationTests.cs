78561fffbf17e6bc8832c035052404c6ba21cc45
Marina Sundström <marna.li@outlook.com>
2025-08-29 00:49:00 +0200

    test: add variable redeclaration tests

---
+++ test/Raven.CodeAnalysis.Tests/Semantics/VariableRedeclarationTests.cs
@@ -0,0 +1,50 @@
+using Raven.CodeAnalysis.Testing;
+using Xunit;
+
+namespace Raven.CodeAnalysis.Semantics.Tests;
+
+public class VariableRedeclarationTests : DiagnosticTestBase
+{
+    [Fact]
+    public void DuplicateGlobalVariable_ReportsDiagnostic()
+    {
+        var code = """
+let x : 'a' | 1 = 1
+let x : 'a' | 1 = 'b'
+""";
+        var verifier = CreateVerifier(code,
+            expectedDiagnostics: [
+                new DiagnosticResult("RAV0167").WithSpan(2, 5, 2, 6).WithArguments("x")
+            ]);
+        verifier.Verify();
+    }
+
+    [Fact]
+    public void DuplicateLocalVariable_ReportsDiagnostic()
+    {
+        var code = """
+func main() {
+    let x : 'a' | 1 = 1
+    let x : 'a' | 1 = 'b'
+}
+""";
+        var verifier = CreateVerifier(code,
+            expectedDiagnostics: [
+                new DiagnosticResult("RAV0167").WithSpan(3, 9, 3, 10).WithArguments("x")
+            ]);
+        verifier.Verify();
+    }
+
+    [Fact]
+    public void ShadowingInInnerBlock_Allowed()
+    {
+        var code = """
+let x : 'a' | 1 = 1
+{
+    let x : 'a' | 1 = 'a'
+}
+""";
+        var verifier = CreateVerifier(code);
+        verifier.Verify();
+    }
+}
