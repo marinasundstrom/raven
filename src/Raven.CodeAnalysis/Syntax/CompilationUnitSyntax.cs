4851b29ab312538ef6dcd3bcddb497b2030db3cf
Marina Sundström <marna.li@outlook.com>
2025-08-28 12:35:16 +0200

    feat: enforce directive order

--- src/Raven.CodeAnalysis/Syntax/CompilationUnitSyntax.cs
+++ src/Raven.CodeAnalysis/Syntax/CompilationUnitSyntax.cs
@@ -2,13 +2,18 @@ namespace Raven.CodeAnalysis.Syntax;
 
 public partial class CompilationUnitSyntax : SyntaxNode
 {
-public CompilationUnitSyntax(SyntaxTree syntaxTree, SyntaxList<ImportDirectiveSyntax> imports, SyntaxList<AliasDirectiveSyntax> aliases, SyntaxList<MemberDeclarationSyntax> members, SyntaxToken endOfFileToken)
-    : base(new InternalSyntax.CompilationUnitSyntax(imports.Green, aliases.Green, members.Green, endOfFileToken.Green), syntaxTree)
-{
-}
+    public CompilationUnitSyntax(SyntaxTree syntaxTree, SyntaxList<ImportDirectiveSyntax> imports, SyntaxList<AliasDirectiveSyntax> aliases, SyntaxList<MemberDeclarationSyntax> members, SyntaxToken endOfFileToken)
+        : base(new InternalSyntax.CompilationUnitSyntax(imports.Green, aliases.Green, members.Green, endOfFileToken.Green), syntaxTree)
+    {
+    }
+
+    private CompilationUnitSyntax(SyntaxTree syntaxTree, InternalSyntax.CompilationUnitSyntax green)
+        : base(green, syntaxTree)
+    {
+    }
 
     internal CompilationUnitSyntax WithSyntaxTree(SyntaxTree syntaxTree)
     {
-    return new CompilationUnitSyntax(syntaxTree, Imports, Aliases, Members, EndOfFileToken);
-}
+        return new CompilationUnitSyntax(syntaxTree, (InternalSyntax.CompilationUnitSyntax)Green);
+    }
 }
