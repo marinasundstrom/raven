4c94e2c9f9c379ee947f10750b5bd548073d0c83
Marina Sundström <marna.li@outlook.com>
2025-08-31 13:47:36 +0200

    fix: adjust token lookup for completions (#344)

--- src/Raven.CodeAnalysis/CompletionService.cs
+++ src/Raven.CodeAnalysis/CompletionService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 using Raven.CodeAnalysis.Syntax;
@@ -18,7 +19,14 @@ public class CompletionService
     /// <returns>A sequence of completion items applicable at the position.</returns>
     public IEnumerable<CompletionItem> GetCompletions(Compilation compilation, SyntaxTree syntaxTree, int position)
     {
-        var token = syntaxTree.GetRoot().FindToken(position);
+        // Completion requests typically originate from the caret position which
+        // lies *after* the character that triggered completion.  Because
+        // FindToken looks for the token that contains the provided position and
+        // token spans are end-exclusive, passing the caret position directly
+        // results in the token to the right being returned.  Adjust the search
+        // position to ensure the token to the left of the caret is used.
+        var searchPosition = Math.Max(0, position - 1);
+        var token = syntaxTree.GetRoot().FindToken(searchPosition);
         var semanticModel = compilation.GetSemanticModel(syntaxTree);
 
         return CompletionProvider.GetCompletions(token, semanticModel, position);
