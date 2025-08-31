a66fd2cb2d30c05fd11ea1505fc0a8f4038042d1
Marina Sundström <marna.li@outlook.com>
2025-08-31 15:06:19 +0200

    Mark failing samples

--- src/Raven.Compiler/samples/build.sh
+++ src/Raven.Compiler/samples/build.sh
@@ -2,13 +2,13 @@
 
 dotnet run -- samples/arrays.rav -o output/arrays.dll
 dotnet run -- samples/enums.rav -o output/enums.dll
-dotnet run -- samples/general.rav -o output/general.dll
+dotnet run -- samples/general.rav -o output/general.dll #error
 dotnet run -- samples/generics.rav -o output/generics.dll
 dotnet run -- samples/io.rav -o output/io.dll
-dotnet run -- samples/test.rav -o output/test.dll
+dotnet run -- samples/test.rav -o output/test.dll #error
 dotnet run -- samples/test2.rav -o output/test2.dll
-dotnet run -- samples/type-unions.rav -o output/type-unions.dll
+dotnet run -- samples/type-unions.rav -o output/type-unions.dll #error
 dotnet run -- samples/tuples.rav -o output/tuples.dll
-dotnet run -- samples/main.rav -o output/main.dll
+dotnet run -- samples/main.rav -o output/main.dll #error
 dotnet run -- samples/classes.rav -o output/classes.dll
 cp ../TestDep/bin/Debug/net9.0/TestDep.dll output/TestDep.dll
