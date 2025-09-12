862ae0f1cdab65e704e0e0d8c82c7450cbb1455a
Marina Sundström <marna.li@outlook.com>
2025-09-12 12:32:38 +0200

    feat: add alias directive completion

--- test/Raven.CodeAnalysis.Tests/Completion/CompletionServiceTests.cs
+++ test/Raven.CodeAnalysis.Tests/Completion/CompletionServiceTests.cs
@@ -160,4 +160,31 @@ import System.Environment.
         Assert.Contains(items, i => i.DisplayText == "SpecialFolder");
         Assert.DoesNotContain(items, i => i.DisplayText == "GetFolderPath");
     }
+
+    [Fact]
+    public void GetCompletions_InAliasDirective_ReturnsMembers()
+    {
+        var code = """
+alias W = System.Console.
+""";
+
+        var syntaxTree = SyntaxTree.ParseText(code);
+
+        var version = TargetFrameworkResolver.ResolveVersion(TestTargetFramework.Default);
+        var refAssembliesPath = TargetFrameworkResolver.GetDirectoryPath(version);
+
+        var compilation = Compilation.Create("test", new CompilationOptions(OutputKind.ConsoleApplication))
+            .AddSyntaxTrees(syntaxTree)
+            .AddReferences([
+                MetadataReference.CreateFromFile(Path.Combine(refAssembliesPath!, "System.Runtime.dll")),
+                MetadataReference.CreateFromFile(typeof(Console).Assembly.Location),
+            ]);
+
+        var service = new CompletionService();
+        var position = code.LastIndexOf('.') + 1;
+
+        var items = service.GetCompletions(compilation, syntaxTree, position).ToList();
+
+        Assert.Contains(items, i => i.DisplayText == "WriteLine");
+    }
 }
