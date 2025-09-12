f39c939fcb033fecb34bc40f8c4e208ee98e7541
Marina Sundström <marna.li@outlook.com>
2025-09-12 10:41:16 +0200

    fix: refocus editor after closing completion

--- test/Raven.Editor.Tests/ProgramTests.cs
+++ test/Raven.Editor.Tests/ProgramTests.cs
@@ -2,6 +2,8 @@ namespace Raven.Editor.Tests;
 
 using System;
 using System.Reflection;
+using Raven.CodeAnalysis;
+using Raven.CodeAnalysis.Syntax;
 using Terminal.Gui;
 
 public class ProgramTests
@@ -23,5 +25,48 @@ public class ProgramTests
         e.Cancel.ShouldBeTrue();
 
         Application.Shutdown();
+    }
+
+    [Fact]
+    public void HideCompletion_RefocusesEditor()
+    {
+        Application.Init(new FakeDriver());
+
+        var workspace = RavenWorkspace.Create();
+        var projectId = workspace.AddProject("TestProject");
+        var documentId = DocumentId.CreateNew(projectId);
+
+        var editor = new TestCodeTextView(workspace, projectId, documentId);
+        Application.Top.Add(editor);
+        editor.SetFocus();
+
+        var completionWin = new Window();
+        Application.Top.Add(completionWin);
+        completionWin.SetFocus();
+
+        typeof(Program).GetField("_editor", BindingFlags.NonPublic | BindingFlags.Static)!
+            .SetValue(null, editor);
+        typeof(Program).GetField("_completionWin", BindingFlags.NonPublic | BindingFlags.Static)!
+            .SetValue(null, completionWin);
+
+        typeof(Program).GetMethod("HideCompletion", BindingFlags.NonPublic | BindingFlags.Static)!
+            .Invoke(null, null);
+
+        Application.Top.Focused.ShouldBe(editor);
+
+        Application.Shutdown();
+    }
+
+    private sealed class TestCodeTextView : CodeTextView
+    {
+        public TestCodeTextView(RavenWorkspace workspace, ProjectId projectId, DocumentId documentId)
+            : base(workspace, projectId, documentId)
+        {
+        }
+
+        public override void OnContentsChanged()
+        {
+            // Skip heavy workspace updates for unit tests
+        }
     }
 }
