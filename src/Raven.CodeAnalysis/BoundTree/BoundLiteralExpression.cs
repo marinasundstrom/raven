5a326d0b62b03b23597cfbd573e764fe32dce445
Marina Sundström <marna.li@outlook.com>
2025-09-16 23:00:39 +0200

    fix: emit default for null literals targeting value types

--- src/Raven.CodeAnalysis/BoundTree/BoundLiteralExpression.cs
+++ src/Raven.CodeAnalysis/BoundTree/BoundLiteralExpression.cs
@@ -4,12 +4,17 @@ internal partial class BoundLiteralExpression : BoundExpression
 {
     public object Value { get; }
 
-    public BoundLiteralExpression(BoundLiteralExpressionKind kind, object value, ITypeSymbol type)
+    public ITypeSymbol? ConvertedType { get; }
+
+    public BoundLiteralExpression(BoundLiteralExpressionKind kind, object value, ITypeSymbol type, ITypeSymbol? convertedType = null)
         : base(type, null, BoundExpressionReason.None)
     {
         Kind = kind;
         Value = value;
+        ConvertedType = convertedType;
     }
+
+    public override ITypeSymbol? GetConvertedType() => ConvertedType;
 
     public BoundLiteralExpressionKind Kind { get; }
 }
--- test/Raven.CodeAnalysis.Tests/CodeGen/NullLiteralEmissionTests.cs
+++ test/Raven.CodeAnalysis.Tests/CodeGen/NullLiteralEmissionTests.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
+using System.Reflection.Emit;
 
 using Raven.CodeAnalysis.Syntax;
 
@@ -6,6 +9,100 @@ namespace Raven.CodeAnalysis.Tests;
 
 public class NullLiteralEmissionTests
 {
+    private static readonly OpCode[] SingleByteOpCodes;
+    private static readonly OpCode[] MultiByteOpCodes;
+
+    static NullLiteralEmissionTests()
+    {
+        SingleByteOpCodes = new OpCode[0x100];
+        MultiByteOpCodes = new OpCode[0x100];
+
+        foreach (var field in typeof(OpCodes).GetFields(BindingFlags.Public | BindingFlags.Static))
+        {
+            if (field.GetValue(null) is OpCode opcode)
+            {
+                var value = (ushort)opcode.Value;
+                if (value < 0x100)
+                {
+                    SingleByteOpCodes[value] = opcode;
+                }
+                else if ((value & 0xFF00) == 0xFE00)
+                {
+                    MultiByteOpCodes[value & 0xFF] = opcode;
+                }
+            }
+        }
+    }
+
+    private static IReadOnlyList<OpCode> GetOpCodes(MethodInfo method)
+    {
+        var body = method.GetMethodBody() ?? throw new InvalidOperationException("Method has no body.");
+        var il = body.GetILAsByteArray() ?? throw new InvalidOperationException("Method body has no IL.");
+        var opcodes = new List<OpCode>();
+
+        for (var i = 0; i < il.Length;)
+        {
+            OpCode opcode;
+            var code = il[i++];
+            if (code == 0xFE)
+            {
+                if (i >= il.Length)
+                    throw new InvalidOperationException("Unexpected end of IL stream when decoding multi-byte opcode.");
+
+                var second = il[i++];
+                opcode = MultiByteOpCodes[second];
+            }
+            else
+            {
+                opcode = SingleByteOpCodes[code];
+            }
+
+            if (opcode.Value == 0 && opcode != OpCodes.Nop)
+                throw new InvalidOperationException($"Unknown opcode: 0x{code:X2}");
+
+            opcodes.Add(opcode);
+
+            switch (opcode.OperandType)
+            {
+                case OperandType.InlineNone:
+                    break;
+                case OperandType.ShortInlineBrTarget:
+                case OperandType.ShortInlineI:
+                case OperandType.ShortInlineVar:
+                    i += 1;
+                    break;
+                case OperandType.InlineVar:
+                    i += 2;
+                    break;
+                case OperandType.InlineI:
+                case OperandType.InlineBrTarget:
+                case OperandType.InlineField:
+                case OperandType.InlineMethod:
+                case OperandType.InlineSig:
+                case OperandType.InlineString:
+                case OperandType.InlineTok:
+                case OperandType.InlineType:
+                    i += 4;
+                    break;
+                case OperandType.InlineI8:
+                case OperandType.InlineR:
+                    i += 8;
+                    break;
+                case OperandType.ShortInlineR:
+                    i += 4;
+                    break;
+                case OperandType.InlineSwitch:
+                    var count = BitConverter.ToInt32(il, i);
+                    i += 4 + (count * 4);
+                    break;
+                default:
+                    throw new NotSupportedException($"Unsupported operand type: {opcode.OperandType}");
+            }
+        }
+
+        return opcodes;
+    }
+
     [Fact]
     public void NullableReference_ReturningNull_EmitsNull()
     {
@@ -36,6 +133,9 @@ class C {
         var value = method.Invoke(instance, Array.Empty<object?>());
 
         Assert.Null(value);
+
+        var opcodes = GetOpCodes(method);
+        Assert.Contains(OpCodes.Ldnull, opcodes);
     }
 
     [Fact]
@@ -68,6 +168,10 @@ class C {
         var value = method.Invoke(instance, Array.Empty<object?>());
 
         Assert.Null(value);
+
+        var opcodes = GetOpCodes(method);
+        Assert.Contains(OpCodes.Initobj, opcodes);
+        Assert.DoesNotContain(OpCodes.Ldnull, opcodes);
     }
 
     [Fact]
@@ -101,5 +205,9 @@ class C {
         var value = method.Invoke(instance, Array.Empty<object?>());
 
         Assert.Null(value);
+
+        var opcodes = GetOpCodes(method);
+        Assert.Contains(OpCodes.Initobj, opcodes);
+        Assert.DoesNotContain(OpCodes.Ldnull, opcodes);
     }
 }
