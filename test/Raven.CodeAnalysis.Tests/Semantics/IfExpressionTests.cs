4f5e7e5ec81cc6d6120d4fcedd5efc37a40c45d6
Marina Sundström <marna.li@outlook.com>
2025-09-18 00:50:08 +0200

    feat: require else for if expressions in value contexts

---
+++ test/Raven.CodeAnalysis.Tests/Semantics/IfExpressionTests.cs
@@ -0,0 +1,37 @@
+using Raven.CodeAnalysis.Testing;
+
+namespace Raven.CodeAnalysis.Semantics.Tests;
+
+public class IfExpressionTests : DiagnosticTestBase
+{
+    [Fact]
+    public void IfExpressionWithoutElse_ReportsDiagnostic()
+    {
+        const string code = """
+let value = if true {
+    42
+}
+""";
+
+        var verifier = CreateVerifier(code,
+            [new DiagnosticResult("RAV1901").WithAnySpan()]);
+
+        verifier.Verify();
+    }
+
+    [Fact]
+    public void IfExpressionWithElse_AllowsAssignment()
+    {
+        const string code = """
+let value = if true {
+    42
+} else {
+    0
+}
+""";
+
+        var verifier = CreateVerifier(code);
+
+        verifier.Verify();
+    }
+}
