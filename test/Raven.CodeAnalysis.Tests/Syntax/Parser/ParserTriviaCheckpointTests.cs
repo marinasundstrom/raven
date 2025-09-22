9f420d3a1dcd7822165f707c24e1d745228c7f3a
Marina Sundström <marna.li@outlook.com>
2025-09-22 22:33:59 +0200

    test: add parser trivia checkpoint coverage

---
+++ test/Raven.CodeAnalysis.Tests/Syntax/Parser/ParserTriviaCheckpointTests.cs
@@ -0,0 +1,108 @@
+using System;
+using System.IO;
+using System.Linq;
+
+using Raven.CodeAnalysis.Syntax;
+using Raven.CodeAnalysis.Syntax.InternalSyntax.Parser;
+
+using Shouldly;
+
+using InternalSyntaxToken = Raven.CodeAnalysis.Syntax.InternalSyntax.SyntaxToken;
+
+namespace Raven.CodeAnalysis.Syntax.Parser.Tests;
+
+public class ParserTriviaCheckpointTests
+{
+    [Fact]
+    public void SingleLineCommentTrivia_PreservedAcrossCheckpoint()
+    {
+        const string code = "let x = 1; // trailing";
+        var context = CreateContextAndReadToToken(code, SyntaxKind.SemicolonToken);
+
+        var positionBeforeCheckpoint = context.Position;
+        context.LastToken.ShouldNotBeNull();
+        context.LastToken!.Kind.ShouldBe(SyntaxKind.SemicolonToken);
+
+        using (context.CreateCheckpoint())
+        {
+            var endOfFile = context.ReadToken();
+            endOfFile.Kind.ShouldBe(SyntaxKind.EndOfFileToken);
+
+            AssertTrivia(endOfFile, SyntaxKind.SingleLineCommentTrivia, "// trailing");
+
+            context.LastToken.ShouldNotBeNull();
+            context.LastToken!.Kind.ShouldBe(SyntaxKind.EndOfFileToken);
+            context.Position.ShouldBeGreaterThan(positionBeforeCheckpoint);
+        }
+
+        context.Position.ShouldBe(positionBeforeCheckpoint);
+        context.LastToken.ShouldNotBeNull();
+        context.LastToken!.Kind.ShouldBe(SyntaxKind.SemicolonToken);
+
+        var endOfFileAfterReset = context.ReadToken();
+        endOfFileAfterReset.Kind.ShouldBe(SyntaxKind.EndOfFileToken);
+        AssertTrivia(endOfFileAfterReset, SyntaxKind.SingleLineCommentTrivia, "// trailing");
+        context.LastToken.ShouldNotBeNull();
+        context.LastToken!.Kind.ShouldBe(SyntaxKind.EndOfFileToken);
+    }
+
+    [Fact]
+    public void MultiLineCommentTrivia_PreservedAcrossCheckpoint()
+    {
+        const string code = "let x = 1; /* trailing */";
+        var context = CreateContextAndReadToToken(code, SyntaxKind.SemicolonToken);
+
+        var positionBeforeCheckpoint = context.Position;
+        context.LastToken.ShouldNotBeNull();
+        context.LastToken!.Kind.ShouldBe(SyntaxKind.SemicolonToken);
+
+        using (context.CreateCheckpoint())
+        {
+            var endOfFile = context.ReadToken();
+            endOfFile.Kind.ShouldBe(SyntaxKind.EndOfFileToken);
+
+            AssertTrivia(endOfFile, SyntaxKind.MultiLineCommentTrivia, "/* trailing */");
+
+            context.LastToken.ShouldNotBeNull();
+            context.LastToken!.Kind.ShouldBe(SyntaxKind.EndOfFileToken);
+            context.Position.ShouldBeGreaterThan(positionBeforeCheckpoint);
+        }
+
+        context.Position.ShouldBe(positionBeforeCheckpoint);
+        context.LastToken.ShouldNotBeNull();
+        context.LastToken!.Kind.ShouldBe(SyntaxKind.SemicolonToken);
+
+        var endOfFileAfterReset = context.ReadToken();
+        endOfFileAfterReset.Kind.ShouldBe(SyntaxKind.EndOfFileToken);
+        AssertTrivia(endOfFileAfterReset, SyntaxKind.MultiLineCommentTrivia, "/* trailing */");
+        context.LastToken.ShouldNotBeNull();
+        context.LastToken!.Kind.ShouldBe(SyntaxKind.EndOfFileToken);
+    }
+
+    private static BaseParseContext CreateContextAndReadToToken(string code, SyntaxKind target)
+    {
+        var context = new BaseParseContext(new Lexer(new StringReader(code)));
+        ReadThroughToken(context, target);
+        return context;
+    }
+
+    private static void ReadThroughToken(BaseParseContext context, SyntaxKind target)
+    {
+        while (true)
+        {
+            var token = context.ReadToken();
+            if (token.Kind == target)
+                return;
+
+            if (token.Kind == SyntaxKind.EndOfFileToken)
+                throw new InvalidOperationException($"Token '{target}' was not found before end of file.");
+        }
+    }
+
+    private static void AssertTrivia(InternalSyntaxToken token, SyntaxKind expectedKind, string expectedText)
+    {
+        var trivia = token.LeadingTrivia.Single(t => t.Kind == expectedKind);
+        trivia.Kind.ShouldBe(expectedKind);
+        trivia.Text.ShouldBe(expectedText);
+    }
+}
