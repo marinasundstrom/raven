0cc3c711f66c7a79d76980270b8b1087595aabef
Marina Sundström <marna.li@outlook.com>
2025-09-25 18:23:34 +0200

    fix: initialize semantic models for all syntax trees (#559)

---
+++ test/Raven.CodeAnalysis.Tests/Semantics/MultipleSyntaxTreeTests.cs
@@ -0,0 +1,45 @@
+using System.Linq;
+
+using Raven.CodeAnalysis;
+using Raven.CodeAnalysis.Syntax;
+using Raven.CodeAnalysis.Tests;
+
+using Xunit;
+
+namespace Raven.CodeAnalysis.Semantics.Tests;
+
+public class MultipleSyntaxTreeTests
+{
+    [Fact]
+    public void MultipleSyntaxTrees_ReferenceAcrossTrees_ProducesNoDiagnostics()
+    {
+        var tree1 = SyntaxTree.ParseText("""
+class Helper {
+    init () {}
+
+    public GetValue() -> int => 42;
+}
+""");
+
+        var tree2 = SyntaxTree.ParseText("""
+class Program {
+    Main() -> unit {
+        let helper = Helper();
+        let value = helper.GetValue();
+        return;
+    }
+}
+""");
+
+        var compilation = Compilation.Create(
+            "app",
+            new[] { tree1, tree2 },
+            TestMetadataReferences.Default,
+            new CompilationOptions(OutputKind.ConsoleApplication));
+
+        var semanticModel = compilation.GetSemanticModel(tree2);
+        var diagnostics = semanticModel.GetDiagnostics();
+
+        Assert.DoesNotContain(diagnostics, d => d.Severity == DiagnosticSeverity.Error);
+    }
+}
