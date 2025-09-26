03ba7bab2f9495d72523484e3c29c0c0042223a9
Marina Sundström <marna.li@outlook.com>
2025-09-26 12:52:14 +0200

    feat: support unicode identifiers

--- src/Raven.CodeAnalysis/Syntax/SyntaxFacts.Identifier.cs
+++ src/Raven.CodeAnalysis/Syntax/SyntaxFacts.Identifier.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 
 namespace Raven.CodeAnalysis.Syntax;
 
@@ -8,5 +9,38 @@ public static partial class SyntaxFacts
     {
         return kind == SyntaxKind.IdentifierToken ||
                (IsKeywordKind(kind) && !IsReservedWordKind(kind));
+    }
+
+    public static bool IsIdentifierStartCharacter(char ch)
+    {
+        if (ch == '_' || ch == '$')
+        {
+            return true;
+        }
+
+        var category = CharUnicodeInfo.GetUnicodeCategory(ch);
+
+        return category == UnicodeCategory.UppercaseLetter ||
+               category == UnicodeCategory.LowercaseLetter ||
+               category == UnicodeCategory.TitlecaseLetter ||
+               category == UnicodeCategory.ModifierLetter ||
+               category == UnicodeCategory.OtherLetter ||
+               category == UnicodeCategory.LetterNumber;
+    }
+
+    public static bool IsIdentifierPartCharacter(char ch)
+    {
+        if (IsIdentifierStartCharacter(ch))
+        {
+            return true;
+        }
+
+        var category = CharUnicodeInfo.GetUnicodeCategory(ch);
+
+        return category == UnicodeCategory.DecimalDigitNumber ||
+               category == UnicodeCategory.ConnectorPunctuation ||
+               category == UnicodeCategory.NonSpacingMark ||
+               category == UnicodeCategory.SpacingCombiningMark ||
+               category == UnicodeCategory.Format;
     }
 }
