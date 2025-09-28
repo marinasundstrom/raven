8c2b382bba95f496f9055fdeee7d5c2a7178c6f8
Marina Sundström <marna.li@outlook.com>
2025-09-28 02:08:50 +0200

    Restore BoundForExpression UnitType property

--- src/Raven.CodeAnalysis/BoundTree/BoundForExpression.cs
+++ src/Raven.CodeAnalysis/BoundTree/BoundForExpression.cs
@@ -7,6 +7,7 @@ internal partial class BoundForExpression : BoundExpression
     public ILocalSymbol Local { get; }
     public BoundExpression Collection { get; }
     public BoundExpression Body { get; }
+    public ITypeSymbol UnitType { get; }
 
     public BoundForExpression(ILocalSymbol local, BoundExpression collection, BoundExpression body, ITypeSymbol unitType)
         : base(unitType, null, BoundExpressionReason.None)
@@ -14,5 +15,6 @@ internal partial class BoundForExpression : BoundExpression
         Local = local;
         Collection = collection;
         Body = body;
+        UnitType = unitType;
     }
 }
