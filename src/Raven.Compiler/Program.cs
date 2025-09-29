2a9365686b89cae2ad6669283967f53a435da319
Marina Sundström <marna.li@outlook.com>
2025-09-29 23:36:27 +0200

    Update sample

--- src/Raven.Compiler/Program.cs
+++ src/Raven.Compiler/Program.cs
@@ -146,7 +146,7 @@ if (showHelp || hasInvalidOption)
 }
 
 if (sourceFiles.Count == 0)
-    sourceFiles.Add($"../../../samples/linq{RavenFileExtensions.Raven}");
+    sourceFiles.Add($"../../../samples/test{RavenFileExtensions.Raven}");
 
 for (int i = 0; i < sourceFiles.Count; i++)
 {
