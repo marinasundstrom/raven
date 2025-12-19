8e31763bac36908009ac7764d068435b702ec495
Marina Sundström <marna.li@outlook.com>
2025-12-19 20:44:28 +0100

    Handle DefaultParameterValueAttribute defaults in metadata

--- src/Raven.CodeAnalysis/Symbols/PE/PEParameterSymbol.cs
+++ src/Raven.CodeAnalysis/Symbols/PE/PEParameterSymbol.cs
@@ -8,6 +8,9 @@ internal partial class PEParameterSymbol : PESymbol, IParameterSymbol
     private readonly TypeResolver _typeResolver;
     private readonly ParameterInfo _parameterInfo;
     private ITypeSymbol _type;
+    private bool _defaultValueComputed;
+    private bool _hasExplicitDefaultValue;
+    private object? _explicitDefaultValue;
 
     public PEParameterSymbol(TypeResolver typeResolver, ParameterInfo parameterInfo, ISymbol containingSymbol, INamedTypeSymbol? containingType, INamespaceSymbol? containingNamespace, Location[] locations)
         : base(containingSymbol, containingType, containingNamespace, locations)
@@ -55,8 +58,8 @@ internal partial class PEParameterSymbol : PESymbol, IParameterSymbol
     {
         get
         {
-            var rawDefaultValue = _parameterInfo.RawDefaultValue;
-            return rawDefaultValue != DBNull.Value && rawDefaultValue != System.Type.Missing;
+            EnsureDefaultValueComputed();
+            return _hasExplicitDefaultValue;
         }
     }
 
@@ -64,11 +67,54 @@ internal partial class PEParameterSymbol : PESymbol, IParameterSymbol
     {
         get
         {
-            var rawDefaultValue = _parameterInfo.RawDefaultValue;
-            if (rawDefaultValue == DBNull.Value || rawDefaultValue == System.Type.Missing)
-                return null;
-
-            return rawDefaultValue;
+            EnsureDefaultValueComputed();
+            return _explicitDefaultValue;
         }
+    }
+
+    private void EnsureDefaultValueComputed()
+    {
+        if (_defaultValueComputed)
+            return;
+
+        var rawDefaultValue = _parameterInfo.RawDefaultValue;
+        if (rawDefaultValue != DBNull.Value && rawDefaultValue != System.Type.Missing)
+        {
+            _hasExplicitDefaultValue = true;
+            _explicitDefaultValue = rawDefaultValue;
+        }
+        else
+        {
+            bool hasOptionalAttribute = false;
+
+            foreach (var attributeData in _parameterInfo.GetCustomAttributesData())
+            {
+                var attributeTypeName = attributeData.AttributeType.FullName;
+
+                if (attributeTypeName == "System.Runtime.InteropServices.DefaultParameterValueAttribute")
+                {
+                    if (attributeData.ConstructorArguments.Count > 0)
+                    {
+                        _hasExplicitDefaultValue = true;
+                        _explicitDefaultValue = attributeData.ConstructorArguments[0].Value;
+                    }
+
+                    break;
+                }
+
+                if (attributeTypeName == "System.Runtime.InteropServices.OptionalAttribute")
+                {
+                    hasOptionalAttribute = true;
+                }
+            }
+
+            if (!_hasExplicitDefaultValue && hasOptionalAttribute)
+            {
+                _hasExplicitDefaultValue = true;
+                _explicitDefaultValue = null;
+            }
+        }
+
+        _defaultValueComputed = true;
     }
 }
