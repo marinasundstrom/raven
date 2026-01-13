deebc59a56f78f5f022b8b3c28adfdfb44776983
Marina Sundström <marna.li@outlook.com>
2026-01-13 10:53:11 +0100

    Add sample

--- src/Raven.Compiler/Program.cs
+++ src/Raven.Compiler/Program.cs
@@ -269,7 +269,7 @@ if (showHelp || hasInvalidOption)
 }
 
 if (sourceFiles.Count == 0)
-    sourceFiles.Add($"../../../../../samples/foo3{RavenFileExtensions.Raven}");
+    sourceFiles.Add($"../../../../../samples/sample1-2{RavenFileExtensions.Raven}");
 
 if (emitDocs && documentationTool == DocumentationTool.RavenDoc && documentationFormatExplicitlySet &&
     documentationFormat == DocumentationFormat.Xml)
