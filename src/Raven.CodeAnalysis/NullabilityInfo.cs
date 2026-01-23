e18802cdb90483b53c0836215ff4ba82b5896126
Marina Sundström <marna.li@outlook.com>
2026-01-23 18:10:06 +0100

    Implement nullability info basics

--- src/Raven.CodeAnalysis/NullabilityInfo.cs
+++ src/Raven.CodeAnalysis/NullabilityInfo.cs
@@ -1,14 +1,37 @@
+using System;
+
 namespace Raven.CodeAnalysis;
 
 [System.Diagnostics.DebuggerDisplay("{GetDebuggerDisplay(), nq}")]
 public readonly struct NullabilityInfo : IEquatable<NullabilityInfo>
 {
+    public NullabilityInfo(NullableAnnotation annotation, NullableFlowState flowState)
+    {
+        Annotation = annotation;
+        FlowState = flowState;
+    }
+
     public NullableAnnotation Annotation { get; }
 
     public NullableFlowState FlowState { get; }
 
     public bool Equals(NullabilityInfo other)
     {
-        throw new NotImplementedException();
+        return Annotation == other.Annotation && FlowState == other.FlowState;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is NullabilityInfo other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine((int)Annotation, (int)FlowState);
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return $"{Annotation} ({FlowState})";
     }
 }
