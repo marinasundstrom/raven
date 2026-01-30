073f67bbcf7f2f5376e42d60f92aa680f2576065
Marina Sundström <marna.li@outlook.com>
2026-01-30 16:36:26 +0100

    Improve attribute name resolution for attribute lists (#1206)
    * Improve attribute name resolution
    
    * Update sample

--- test/Raven.CodeAnalysis.Tests/Semantics/AttributeBindingTests.cs
+++ test/Raven.CodeAnalysis.Tests/Semantics/AttributeBindingTests.cs
@@ -31,4 +31,29 @@ class C { }
         Assert.Equal("use new", attribute.ConstructorArguments.Single().Value);
         Assert.Empty(compilation.GetDiagnostics());
     }
+
+    [Fact]
+    public void AttributeNameIsResolvedAfterTopLevelStatements()
+    {
+        const string source = """
+import System.*
+import System.Console.*
+
+Console.WriteLine("Test");
+
+[Obsolete("Hello")]
+class Foo { }
+""";
+
+        var (compilation, tree) = CreateCompilation(source, new CompilationOptions(OutputKind.ConsoleApplication));
+        var model = compilation.GetSemanticModel(tree);
+        var classDeclaration = tree.GetRoot().DescendantNodes().OfType<ClassDeclarationSyntax>().Single();
+        var type = (INamedTypeSymbol)model.GetDeclaredSymbol(classDeclaration)!;
+
+        var attribute = Assert.Single(type.GetAttributes());
+
+        Assert.Equal("ObsoleteAttribute", attribute.AttributeClass?.Name);
+        Assert.Equal("Hello", attribute.ConstructorArguments.Single().Value);
+        Assert.Empty(compilation.GetDiagnostics());
+    }
 }
