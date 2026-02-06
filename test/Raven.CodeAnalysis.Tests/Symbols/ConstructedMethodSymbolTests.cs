f5673ffee367f8b64bbe8db20811ed36741a0a4d
Marina Sundström <marna.li@outlook.com>
2026-02-06 22:19:49 +0100

    Add ConstructedMethodSymbolTests

--- test/Raven.CodeAnalysis.Tests/Symbols/ConstructedMethodSymbolTests.cs
+++ test/Raven.CodeAnalysis.Tests/Symbols/ConstructedMethodSymbolTests.cs
@@ -122,4 +122,111 @@ class Outer<T>
         var fullyQualified = find.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat);
         Assert.Equal("public System.Collections.Generic.List<string>.Find(match: System.Predicate<string>) -> string", fullyQualified);
     }
+
+    [Fact]
+    public void ConstructedMethod_SubstitutesMethodTypeParametersInNestedReturnType()
+    {
+        var source = """
+class Outer<T> {
+    public class Inner {
+        public init(value: T) { }
+    }
+}
+
+class Factory {
+    public static Create<U>(value: U) -> Outer<U>.Inner {
+        return Outer<U>.Inner(value)
+    }
+}
+""";
+
+        var syntaxTree = SyntaxTree.ParseText(source);
+        var compilation = Compilation.Create(
+                "constructed-method-nested-return",
+                [syntaxTree],
+                TestMetadataReferences.Default,
+                new CompilationOptions(OutputKind.ConsoleApplication));
+
+        var model = compilation.GetSemanticModel(syntaxTree);
+        var root = syntaxTree.GetRoot();
+        var factorySyntax = root.DescendantNodes()
+            .OfType<ClassDeclarationSyntax>()
+            .Single(c => c.Identifier.Text == "Factory");
+        var outerSyntax = root.DescendantNodes()
+            .OfType<ClassDeclarationSyntax>()
+            .Single(c => c.Identifier.Text == "Outer");
+
+        var factoryDefinition = Assert.IsAssignableFrom<INamedTypeSymbol>(model.GetDeclaredSymbol(factorySyntax));
+
+        var createDefinition = Assert.Single(factoryDefinition.GetMembers("Create").OfType<IMethodSymbol>());
+
+        var intType = compilation.GetSpecialType(SpecialType.System_Int32);
+        var constructedCreate = Assert.IsAssignableFrom<IMethodSymbol>(createDefinition.Construct(intType));
+
+        var returnType = Assert.IsAssignableFrom<INamedTypeSymbol>(constructedCreate.ReturnType);
+        var outerDefinition = Assert.IsAssignableFrom<INamedTypeSymbol>(model.GetDeclaredSymbol(outerSyntax));
+        var outerInt = Assert.IsAssignableFrom<INamedTypeSymbol>(outerDefinition.Construct(intType));
+        Assert.Equal("Inner", returnType.Name);
+        Assert.True(SymbolEqualityComparer.Default.Equals(outerInt, returnType.ContainingType));
+        Assert.Equal("Outer<int>.Inner", returnType.ToDisplayString(SymbolDisplayFormat.RavenErrorMessageFormat));
+        Assert.Collection(
+            constructedCreate.Parameters,
+            parameter => Assert.True(SymbolEqualityComparer.Default.Equals(intType, parameter.Type)));
+    }
+
+    [Fact]
+    public void ConstructedMethod_SubstitutesMethodTypeParametersInNestedGenericReturnType()
+    {
+        var source = """
+class Outer<T> {
+    public class Inner<V> {
+        public init(value: T, other: V) { }
+    }
+}
+
+class Factory {
+    public static Create<U, V>(value: U, other: V) -> Outer<U>.Inner<V> {
+        return Outer<U>.Inner<V>(value, other)
+    }
+}
+""";
+
+        var syntaxTree = SyntaxTree.ParseText(source);
+        var compilation = Compilation.Create(
+                "constructed-method-nested-generic-return",
+                [syntaxTree],
+                TestMetadataReferences.Default,
+                new CompilationOptions(OutputKind.ConsoleApplication));
+
+        var model = compilation.GetSemanticModel(syntaxTree);
+        var root = syntaxTree.GetRoot();
+        var factorySyntax = root.DescendantNodes()
+            .OfType<ClassDeclarationSyntax>()
+            .Single(c => c.Identifier.Text == "Factory");
+        var outerSyntax = root.DescendantNodes()
+            .OfType<ClassDeclarationSyntax>()
+            .Single(c => c.Identifier.Text == "Outer");
+
+        var factoryDefinition = Assert.IsAssignableFrom<INamedTypeSymbol>(model.GetDeclaredSymbol(factorySyntax));
+
+        var createDefinition = Assert.Single(factoryDefinition.GetMembers("Create").OfType<IMethodSymbol>());
+
+        var intType = compilation.GetSpecialType(SpecialType.System_Int32);
+        var stringType = compilation.GetSpecialType(SpecialType.System_String);
+        var constructedCreate = Assert.IsAssignableFrom<IMethodSymbol>(createDefinition.Construct(intType, stringType));
+
+        var returnType = Assert.IsAssignableFrom<INamedTypeSymbol>(constructedCreate.ReturnType);
+        var outerDefinition = Assert.IsAssignableFrom<INamedTypeSymbol>(model.GetDeclaredSymbol(outerSyntax));
+        var outerInt = Assert.IsAssignableFrom<INamedTypeSymbol>(outerDefinition.Construct(intType));
+        Assert.Equal("Inner", returnType.Name);
+        Assert.True(SymbolEqualityComparer.Default.Equals(outerInt, returnType.ContainingType));
+        Assert.Collection(
+            returnType.TypeArguments,
+            argument => Assert.True(SymbolEqualityComparer.Default.Equals(stringType, argument)));
+
+        Assert.Collection(
+            constructedCreate.Parameters,
+            parameter => Assert.True(SymbolEqualityComparer.Default.Equals(intType, parameter.Type)),
+            parameter => Assert.True(SymbolEqualityComparer.Default.Equals(stringType, parameter.Type)));
+    }
 }
